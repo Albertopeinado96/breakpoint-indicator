2ac90a0ce16a9663542694742227ddf79bb5717f
Alberto <albertopeisan@gmail.com>
2021-08-08 16:52:41 +0100

    Add max possible index to the indicator

--- breakpoint-indicator.blade.php
+++ breakpoint-indicator.blade.php
@@ -1,6 +1,6 @@
 <div>
     <!-- Active Breakpoint Indicator -->
-    <div class="fixed top-0 right-0 mt-16 m-4 p-3 text-xl font-mono text-white h-8 w-8 rounded-full flex items-center justify-center bg-gray-700 sm:bg-pink-500 md:bg-yellow-500 lg:bg-green-500 xl:bg-blue-500">
+    <div class="fixed top-0 right-0 ml-12 m-4 p-3 text-xl font-mono text-white h-8 w-8 rounded-full flex items-center justify-center bg-gray-700 sm:bg-pink-500 md:bg-yellow-500 lg:bg-green-500 xl:bg-blue-500" style="z-index: 999">
         <div class="block sm:hidden md:hidden lg:hidden xl:hidden">al</div>
         <div class="hidden sm:block md:hidden lg:hidden xl:hidden">sm</div>
         <div class="hidden sm:hidden md:block lg:hidden xl:hidden">md</div>
